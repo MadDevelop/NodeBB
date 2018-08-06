ac5311d637774531692c2410a02c86e93e9f7864
MadDev <maddev@rspeer.org>
2018-08-05 22:47:08 -0500

    test

--- Plugins/RsPeer/index.js
+++ Plugins/RsPeer/index.js
@@ -6,24 +6,7 @@ const apiUrl = Buffer.from(nconf.get('rspeerApi')).toString('base64');
 
 const RsPeer = {
     onImageUpload: imageUploadService.onImageUpload,
-    onAppLoad : (params, callback) => {
-        params.router.use(async (req, res, next) => {
-        	console.log(req.path);
-        	if(req.path === "/categories" || req.path === '/') {
-				res.cookie('sso_url', ssoUrl);
-				res.cookie('api_url', apiUrl);
-				const loginToken = req.query.idToken;
-				console.log("Logging in.");
-				if(loginToken) {
-					await loginService.loginWithToken(req, req.query.idToken, next);
-				}
-				next();
-			} else {
-        		next();
-			}
-        });
-        callback();
-    }
+	onLogin : loginService.onRouteChange
 };
 
 module.exports = RsPeer;
