6d37343ab8e040949219e18b1f9ad757e3afe3ee
MadDev <maddev@rspeer.org>
2018-08-05 22:34:47 -0500

    Updated login sso

--- Plugins/RsPeer/index.js
+++ Plugins/RsPeer/index.js
@@ -1,18 +1,27 @@
 const imageUploadService = require('./services/image_upload_service');
 const loginService = require('./services/login_service');
 const nconf = module.parent.require('nconf');
-
+const ssoUrl = Buffer.from(nconf.get('ssoPath')).toString('base64');
+const apiUrl = Buffer.from(nconf.get('rspeerApi')).toString('base64');
 
 const RsPeer = {
     onImageUpload: imageUploadService.onImageUpload,
     onAppLoad : (params, callback) => {
         params.router.use(async (req, res, next) => {
-            res.cookie('sso_url', Buffer.from(nconf.get('ssoPath')).toString('base64'));
-            res.cookie('api_url', Buffer.from(nconf.get('rspeerApi')).toString('base64'));
-            const loginToken = req.query.idToken;
-            loginToken
-                ? loginService.loginWithToken(req, req.query.idToken, next)
-                : next()
+        	console.log(req.path);
+        	if(req.path === "/categories" || req.path === '/') {
+				res.cookie('sso_url', ssoUrl);
+				res.cookie('api_url', apiUrl);
+				const loginToken = req.query.idToken;
+				console.log("Logging in.");
+				if(loginToken) {
+					await loginService.loginWithToken(req, req.query.idToken, next);
+				} else {
+					next();
+				}
+			} else {
+        		next();
+			}
         });
         callback();
     }
